dd16d5432bde94f95aabce5ebc0ab9fabc795c2c
pipe2442 <felipeangulo2442@gmail.com>
2021-04-17 22:37:54 -0500

    Create list to display user friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @friendship = Friendship.new
   end
 
   def show
+    @friendship = Friendship.new
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
