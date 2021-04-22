7c89c9b78f3c47c479858a4c90c9237c528c0c7d
pipe2442 <felipeangulo2442@gmail.com>
2021-04-21 19:04:33 -0500

    Add integration test for bidirectional friendship relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-  scope :current_user_friends, -> { where('user_id == ?', current_user.id) }
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
